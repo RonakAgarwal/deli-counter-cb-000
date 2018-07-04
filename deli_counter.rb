af26135768381bb2611a0aa0b454593f1c35d96e
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-04 18:16:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 #line
 def line(katz_deli)
   phrase = "The line is currently: "
-  if katz_deli.length > 0 
+  if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
       phrase += "#{index + 1}. #{name} "
     end
@@ -24,7 +24,7 @@ end
 def now_serving(katz_deli)
   if katz_deli == 0
     puts "There is nobody waiting to be served!"
-  else 
+  else
 	 puts "Currently serving " + katz_deli[0] +"."
   end
   katz_deli.shift
